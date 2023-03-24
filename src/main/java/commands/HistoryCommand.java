c441df41a3fb9e404aa70c942a307d46d4729dc5
Dmitry Venin <dmvenin@gmail.com>
2023-03-24 18:26:34 +0300

    Recommit

--- src/main/java/commands/HistoryCommand.java
+++ src/main/java/commands/HistoryCommand.java
@@ -3,6 +3,8 @@ package commands;
 import console.Console;
 import utility.CommandHistory;
 
+import java.util.LinkedList;
+
 public class HistoryCommand extends Command {
     public HistoryCommand(Console console) {
         super("history", "Prints out the last " + CommandHistory.HISTORY_SIZE + " executed commands", console);
@@ -10,8 +12,9 @@ public class HistoryCommand extends Command {
 
     @Override
     public boolean execute() {
-        for (Command command : console.getCommandHistory()) {
-            console.writeLine(command.getName());
+        LinkedList<Command> commandHistory = console.getCommandHistory();
+        for (Command command : commandHistory) {
+            console.writeLine(commandHistory.indexOf(command) + " " + command.getName());
         }
         return true;
     }
