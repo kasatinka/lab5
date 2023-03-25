1b400bd45aa5a528d5102c4f9622a4480a374f3f
Dmitry Venin <dmvenin@gmail.com>
2023-03-25 15:47:53 +0300

    Recommit

--- src/main/java/utility/CollectionManager.java
+++ src/main/java/utility/CollectionManager.java
@@ -17,6 +17,8 @@ import java.util.Deque;
                   |         validation logic
                   |
  *            XMLHandler
+
+                  =)
  * */
 
 public class CollectionManager {
