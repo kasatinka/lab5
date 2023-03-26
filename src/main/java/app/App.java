ca9369c6e7db775384826d7e8d7780c1bae43690
Dmitry Venin <dmvenin@gmail.com>
2023-03-26 16:31:59 +0300

    Recommit

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -7,12 +7,10 @@ import java.util.Scanner;
 
 public class App {
     private String fileName;
-    private CollectionManager collectionManager;
     private Console console;
 
     public App(String fileName) {
         this.fileName = fileName;
-        this.collectionManager = new CollectionManager(fileName);
         this.console = new Console(new Scanner(System.in), fileName);
     }
 
