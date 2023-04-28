05affdf88f75eec8ce71eaa82c87bc8c3583f30f
Dmitry Venin <dmvenin@gmail.com>
2023-04-28 13:42:58 +0300

    Recommit

--- src/main/java/utility/FileManager.java
+++ src/main/java/utility/FileManager.java
@@ -10,15 +10,21 @@ import java.util.Deque;
 import java.util.Scanner;
 
 public class FileManager {
+    private final XMLHandler xmlHandler = new XMLHandler();
     private Scanner fileScanner;
     private String fileName;
-    private final XMLHandler xmlHandler = new XMLHandler();
     private File file;
+    private FileWriter fileWriter;
 
     public FileManager(String fileName) {
         this.fileName = fileName;
         this.file = new File(fileName);
         this.fileScanner = new Scanner(fileName);
+        try {
+            this.fileWriter = new FileWriter(file);
+        } catch (IOException e) {
+            System.out.println("Can't access file");
+        }
     }
 
     public String save(Deque<SpaceMarine> marinesCollection) {
@@ -38,5 +44,13 @@ public class FileManager {
         ArrayDeque<SpaceMarine> d = new ArrayDeque<>();
 
         return d;
+    }
+
+    private void writeXMLHeader() {
+
+    }
+
+    private void writeXMLFooter() {
+
     }
 }
